0f7cdb1669eb0ca49ed96476ef56971afca08f81
Aleksandr <aleksandr-kvid@yandex.ru>
2021-09-01 12:07:03 +0300

    Add Task2

--- Lesson03/Functions.py
+++ Lesson03/Functions.py
@@ -2,11 +2,11 @@
 Функции по всем заданиям урока 3 вынесены в отдельный модуль
 Каждая функция подключается непосредственно в задании с помощью инструкций from и import
 ====================================================================="""
-# Добавление абстрактного типа данных number_types: int, float, complex numbers
+# ========== Добавление абстрактного типа данных number_types: int, float, complex numbers
 import number_types as number_types
 # number_types = (int, float, complex)
 
-
+# ========== Функция нахождения частного ==========
 def user_division(dividend: number_types, divider: number_types) -> number_types:
     """
     Делит число на число
@@ -15,3 +15,12 @@ def user_division(dividend: number_types, divider: number_types) -> number_types
     :return: Частное от деления
     """
     return dividend / divider
+
+# ========== Функция вывода информации по пользователю ==========
+def print_user_data(**user_data) -> None:
+    """ Распечатывает в одну строку данные пользователя
+    :param user_data: данные пользователя
+    """
+    print(f'ИМЯ: {user_data.get("name")}; ФАМИЛИЯ: {user_data.get("surname")}; '
+          f'ГОД РОЖДЕНИЯ: {user_data.get("birth_year")}; ГОРОД ПРОЖИВАНИЯ: {user_data.get("city")}; '
+          f'EMAIL: {user_data.get("email")}; ТЕЛЕФОН: {user_data.get("phone")}')
